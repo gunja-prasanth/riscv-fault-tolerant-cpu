b930e2cece43397d02b3c5ed061efe206bfb1228
Prasanth <gunjaprasanth693@gmail.com>
2026-02-06 16:59:30 +0530

    Day-25: Add estimation layer architecture and README

---
+++ rtl/estimation/estimation_top/estimation_top.sv
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06.02.2026 16:38:37
+// Design Name: 
+// Module Name: estimation_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module estimation_top (
+    input  logic clk,
+    input  logic reset,
+
+    // tapped signals (read-only)
+    input  logic pc_write,
+    input  logic reg_write,
+    input  logic mem_write,
+    input  logic recovery_active
+);
+
+
+endmodule
